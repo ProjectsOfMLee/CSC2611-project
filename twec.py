648481cf24f79bf8ada484dc05453a961ff5d4b2
M. L <104241750+ProjectsOfMLee@users.noreply.github.com>
2022-10-25 15:03:24 -0400

    Update twec.py

--- twec.py
+++ twec.py
@@ -1,7 +1,7 @@
 from twec.twec import TWEC
 from gensim.models.word2vec import Word2Vec
 
-aligner = TWEC(size=30, siter=10, diter=10, workers=4)
+aligner = TWEC(size=30, siter=10, diter=10, workers=4) # 超参数，可调&作图对比
 
 # train the compass: the text should be the concatenation of the text from the slices
 aligner.train_compass("examples/training/compass.txt", overwrite=False) 
@@ -9,12 +9,12 @@ aligner.train_compass("examples/training/compass.txt", overwrite=False)
 
 # now you can train slices and they will be already aligned
 # these are gensim word2vec objects
-slice_one = aligner.train_slice("examples/training/arxiv_14.txt", save=True)
-slice_two = aligner.train_slice("examples/training/arxiv_9.txt", save=True)
+slice_one = aligner.train_slice("examples/training/1994.txt", save=True)
+slice_two = aligner.train_slice("examples/training/2021.txt", save=True)
 
 # load trained models
-model1 = Word2Vec.load("model/arxiv_14.model")
-model2 = Word2Vec.load("model/arxiv_9.model")
+model1 = Word2Vec.load("model/1994.model")
+model2 = Word2Vec.load("model/2021.model")
 
 """
 # -*- coding: utf-8 -*-
